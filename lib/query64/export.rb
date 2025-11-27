be5a1356388ff94ad19deefdd91193839285deac
Jules Debeaumont <jdebeaumont@chu-reims.fr>
2025-11-27 16:17:33 +0100

    [ADD] export

--- lib/query64/export.rb
+++ lib/query64/export.rb
@@ -10,12 +10,44 @@ module Query64
             return ""
           end
 
+          json_key_column = {}
+          json_key_association = Set.new
+          query64_params[:columnsToDisplay].each do |column_to_display|
+            if column_to_display.exclude?('.')
+              next
+            end
+            association_name = column_to_display.split('.')[0] || ''
+            json_key_association.add(association_name)
+            if (json_key_column[association_name.to_sym].nil?)
+              json_key_column[association_name.to_sym] = []
+            end
+            json_key_column[association_name.to_sym] << (column_to_display.split('.')[1] || '')
+          end
+
           csv_content = CSV.generate(headers: true, col_sep: ';') do |csv|
-            header_row = data_rows.first.keys
+            header_row = data_rows.first.keys.filter do |key|
+              json_key_association.exclude?(key)
+            end
+            json_key_column.entries.each do |entry|
+              entry[1].each do |json_col_name|
+                header_row << "#{entry[0]} : #{json_col_name}"
+              end
+            end
             csv << header_row
 
             data_rows.each do |data_row|
-              row = data_row.values
+              row = []
+              data_row.each do |entry|
+                json_assoc = json_key_association.find(entry[0])
+                if json_assoc
+                  json_key_column[json_assoc.to_sym].each do |json_col_name|
+                    row << entry[1][json_col_name]
+                  end
+                else
+                  row << entry[1]
+                end
+              end
+
               csv << row
             end
           end
