86c3cfadaa079edffccaf7b0837a2bc3788cb0b9
Jules Debeaumont <jdebeaumont@chu-reims.fr>
2025-12-10 15:28:42 +0100

    [FIX] fix order by with join

--- lib/query64/builder.rb
+++ lib/query64/builder.rb
@@ -413,7 +413,7 @@ module Query64
           end
           table_alias_sub_request = join_data[:alias_label]
           sort_clauses_sub_request_array << "#{table_alias_sub_request}.#{column_name} #{sort[:sort]}"
-          if join_data[:enabled_for_sub_request]
+          if !join_data[:enabled_for_sub_request]
             sort_clauses_array << "#{table_alias_sub_request}.#{column_name} #{sort[:sort]}" # ???
           end
         else
